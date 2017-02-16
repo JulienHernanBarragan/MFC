5aed736dc6873074cf73cad061919488a68b9dac
Hernandez Julien <julienhernandez14@gmail.com>
2017-02-16 12:01:12 +0100

    Import Application

---
+++ MFC_BTS_SIO/src/interfaceGraphique/Identification.java
@@ -0,0 +1,103 @@
+package interfaceGraphique;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import traitementDB.Connect_DB;
+
+public class Identification extends JFrame {
+		
+	private static final long serialVersionUID = 1L;
+	JPanel pan;
+	JLabel logo, utilisateur, password;
+	JTextField TFutilisateur;
+	JPasswordField TFpassword;
+	JButton Bquitter, Bconnexion;
+	String Dutilisateur,Dmdp,texteID, Duser;
+		
+	public Identification() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Identification.class.getResource("/image/logo MFC.png")));
+		setTitle("Application de gestion du centre de formation MFC");
+		pan = new JPanel();
+	    pan.setLayout(null);
+		setSize(700,400);
+		setContentPane(pan);
+		setLocationRelativeTo(null);
+			
+		logo = new JLabel();
+		logo.setIcon(new ImageIcon(Identification.class.getResource("/image/logomfc.png")));
+		logo.setBounds(300, 11, 163, 128);
+		pan.add(logo);
+			
+		utilisateur = new JLabel("Utilisateur :");
+		utilisateur.setFont(new Font("Times New Roman", Font.BOLD, 21));
+		utilisateur.setBounds(86, 142, 151, 24);
+		pan.add(utilisateur);
+			
+		password = new JLabel ("Mot de Passe :");
+		password.setFont(new Font("Times New Roman", Font.BOLD, 21));
+		password.setBounds(86, 198, 142, 24);
+		pan.add(password);
+			
+		TFutilisateur = new JTextField();
+		TFutilisateur.setBounds(250, 145, 275, 26);
+		TFutilisateur.setColumns(10);
+		pan.add(TFutilisateur);
+			
+		TFpassword = new JPasswordField();
+		TFpassword.addKeyListener(new KeyAdapter() {
+			@SuppressWarnings("deprecation")
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
+					Dutilisateur = TFutilisateur.getText();
+		    		Dmdp = TFpassword.getText();
+		    		String admin = new Connect_DB().verifID(Dutilisateur, Dmdp);
+		    			if (admin == "accept") {
+		    				Menu session = new Menu();
+		    				session.setVisible(true);
+		    				dispose();
+		    			}
+				}
+			}
+		});
+		TFpassword.setBounds(250, 200, 275, 26);
+		TFpassword.setColumns(10);
+		pan.add(TFpassword);
+			
+		Bconnexion = new JButton ("Connexion");
+		Bconnexion.setBackground(new Color(2, 152, 229));
+		Bconnexion.setForeground(Color.WHITE);
+		Bconnexion.setFont(new Font("Trebuchet MS", Font.PLAIN, 30));
+		Bconnexion.setBounds(229, 265, 257, 46);
+		Bconnexion.addActionListener(new ActionListener()  {
+			@SuppressWarnings("deprecation")
+			public void actionPerformed(ActionEvent e)  {
+	    		Dutilisateur = TFutilisateur.getText();
+	    		Dmdp = TFpassword.getText();
+	    		String admin = new Connect_DB().verifID(Dutilisateur, Dmdp);
+	    			if (admin == "accept") {
+	    				Menu session = new Menu();
+	    				session.setVisible(true);
+	    				dispose();
+	    			} 
+		    	}
+			});
+			pan.add(Bconnexion);
+			
+		}
+}
